9d18ea9ee8bfbb7e6920e246b2ee13a5da3f651b
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-04 16:19:01 -0500

    @kapham2 Done without Bonus

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+  def initialize(props = {})
+    @name = props[:name]
+    @type= props[:type]
+    @id = props[:id]
+    @db = props[:db]
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+  end
+
+  def self.find(id_n,db)
+    id = db.execute("SELECT id FROM pokemon WHERE id = ?", id_n).flatten.first
+    name = db.execute("SELECT name FROM pokemon WHERE id = ?", id_n).flatten.first
+    type = db.execute("SELECT type FROM pokemon WHERE id = ?", id_n).flatten.first
+    pokemon_hash = {:id => id, :name => name, :type => type, :db => db}
+    Pokemon.new(pokemon_hash)
+  end
 end
