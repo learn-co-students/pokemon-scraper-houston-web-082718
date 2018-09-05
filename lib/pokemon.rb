28939fa4824747b8f9a1be3a55f424de3bb1d2a5
Naeblis <johnmlowery@gmail.com>
2018-09-05 16:03:00 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,46 +2,53 @@ require 'pry'
 
 class Pokemon
 
-    attr_accessor :name, :type, :id, :db
+    attr_accessor :name, :type, :id, :db, :hp
 
-    def initialize(name:, type:, id:, db:)
-        @name = name
-        @type = type
-        @db = db
-        @id = id
-        # @hp = hp
+    def initialize (props={})
+        @name = props['name']
+        @type = props['type']
+        @id = props['id']
+        @db = props['db']
+        @hp = props['hp']
     end
 
+    def self.save (name, type, db)
 
-    def self.save(name, type, db)
-
-        sql = <<-SQL 
-        INSERT INTO pokemon (name, type) VALUES (?, ?); 
-        SQL
-        db.execute(sql, name, type)
-    end
-
-    def self.find(id, db)
         sql = <<-SQL
-            SELECT * FROM pokemon WHERE id = ?;
+            INSERT INTO pokemon (name, type) VALUES (?, ?);
         SQL
-        test = db.execute(sql, id)
+
+        db.execute(sql, name, type)
         # binding.pry
-        Pokemon.new(name: test.first[1], type: test.first[2], db: db, id: test.first[0])
     end
 
-    # def alter_hp(hp, db)
-    #     sql = <<-SQL
-    #     UPDATE pokemon SET hp = ? WHERE name = 'Pikachu';
-    #     SQL
+    def self.find (id, db)
 
-    #     poki_hp = db.execute(sql, hp)
+        sql = <<-SQL
+            SELECT * FROM pokemon WHERE id = ?
+        SQL
 
-    #     # Pokemon.new('' => test.first[1], 'id' => test.first[0], 'type' => test.first[2], )
-    #     # binding.pry
-# 
-        # end
-    
+        found = db.execute(sql, id)
 
-    
+        poke = found[0]
+
+        new_poke = Pokemon.new('name' => poke[1], 'type' => poke[2], 'id' => poke[0], 'db' => db, 'hp' => poke[3])
+
+        # binding.pry
+
+    end
+
+    def alter_hp (hp, db)
+
+        sql = <<-SQL
+            UPDATE pokemon SET hp = ? WHERE id = ?;
+        SQL
+
+        # binding.pry
+
+        db.execute(sql, hp, self.id)
+
+    end
+
+
 end
